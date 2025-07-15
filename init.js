52c950491a3c60f6500eb4f920b41bf486c68d3c
siriusmart <siriusmart@tuta.io>
2025-07-15 21:26:24 +0100

    initial commit

---
+++ init.js
@@ -0,0 +1,65 @@
+let { StringArgumentType } = com.mojang.brigadier.arguments;
+let { createText } = require("text");
+let command = require("command");
+
+addEventListener("clientModifyReceiveGameMessageEvent", (text) => {
+    let content = text.getString().replaceAll(/§./g, "");
+    if (content.trim().length == 0) return;
+
+    let words = content.split(" ");
+    let isPlayer = /<.*>|.*:/.test(words[0]);
+
+    if (isPlayer) content = words.slice(1).join(" ");
+
+    return createText([
+        text,
+        " ",
+        {
+            content: "[T]",
+            color: isPlayer ? "#cba6f7" : "#6c7086",
+            hover: `Translate "${content}"`,
+            click: `/translate auto ${content}`,
+        },
+    ]);
+});
+
+function autoTranslate(ctx) {
+    let content = StringArgumentType.getString(ctx, "content");
+    console.print("\u00A78Translating...");
+    fetch("https://translate.siri.ws/translate", {
+        method: "POST",
+        body: JSON.stringify({
+            q: content,
+            source: "auto",
+            target: "en",
+            format: "text",
+            alternatives: 3,
+            api_key: "",
+        }),
+        headers: { "Content-Type": "application/json" },
+    })
+        .then((res) => {
+            let json = res.json();
+            if (json.error) {
+                console.error(`Could not translate message: ${json.error}`);
+                return;
+            }
+            console.print(`\u00A77Translated: ${json.translatedText}`);
+        })
+        .catch((e) => console.error(`Could not translate message: ${e}`));
+}
+
+command.register({
+    name: "translate",
+
+    subcommands: {
+        auto: {
+            args: {
+                content: {
+                    type: StringArgumentType.greedyString(),
+                    execute: autoTranslate,
+                },
+            },
+        },
+    },
+});
